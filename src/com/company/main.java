648eb95c5024dced13eb3afd880e5161a4a7f83f
Moneer.raheen <moneer.raheen@gmail.com>
2021-03-07 19:53:42 -0500

    my second commit

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -4,7 +4,7 @@ public class main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <4 ; i++) {
             System.out.println("Hello batch 8");
             Thread.sleep(2000);
         }
